2640e9a7f4eaf769e78ac8c466d73708090973bf
Eric Schubert <49171315+bernardericschubert@users.noreply.github.com>
2019-07-17 15:01:17 -0700

    Updating for Heroku deploy

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,4 +9,9 @@ module.exports = function(app,path) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+
+  // Adding / for Heroku
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 };
